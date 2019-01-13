4e9faa8b789c13437241816c102c5e88e8722cde
silentgamelan <28412081+SilentGamelan@users.noreply.github.com>
2019-01-13 01:57:54 +0000

    Fixes connecting timestamp app and adds basic logger

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const app = express();
 const router = require('./fcc-timestamp');
 const port = 3000;
 
-
-
 // NOTE: to use template strings, use *backticks* not ''
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
-app.use('./fcc-timestamp', router);
+// logger middleware
+app.use((req, res, next) => {
+    let log = req.method + " " + req.path + "-" + req.ip;
+    console.log(log);
+    next();
+});
+
+app.get('/', function(req, res) {
+    res.send('hello, world!');
+});
+
+app.use('/api/timestamp', router);
