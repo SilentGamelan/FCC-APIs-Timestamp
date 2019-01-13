ec6f9cc6375317b5c55acac38d2c972597e1fffa
silentgamelan <28412081+SilentGamelan@users.noreply.github.com>
2019-01-13 16:50:01 +0000

    Adds root dir routing and fixes routing param syntax

--- fcc-timestamp.js
+++ fcc-timestamp.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 
+router.get('/', function(req, res, next) {
+    res.send('Validate your timestamp at /api/timestamp/[yourTimestamp]');
+    next();
+});
 
-router.get('/', function(req, res) {
-    res.send('Timestamp goes here');
+router.get('/api/timestamp/:timeToValidate', function(req, res) {
+    res.send(req.params.timeToValidate);
 });
 
 
